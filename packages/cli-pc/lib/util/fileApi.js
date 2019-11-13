990b8935ae71ea60e4d6387bfe94500124077773
huning <304873772@qq.com>
2019-11-13 17:25:17 +0800

    feat: 增加对文件的操作api

---
+++ packages/cli-pc/lib/util/fileApi.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+/**
+ * 读取文件
+ *
+ * @param {*} path
+ * @returns
+ */
+exports.readFile = (path) => {
+  const finalTemplate = fs.readFileSync(path, 'utf-8')
+  return finalTemplate
+}
+
+/**
+ * 解析文件
+ *
+ * @param {*} template
+ * @param {*} options
+ * @returns
+ */
+exports.formatTemplate = (template, options) => {
+  return template
+}
+
+/**
+ * 渲染文件
+ *
+ * @param {*} template
+ * @param {*} { exportPath, fileName }
+ */
+exports.renderTemplate = (template, { exportPath, fileName }) => {
+
+}
